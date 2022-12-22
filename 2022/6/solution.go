59f05a623bab17cc949dd680814c6ea2ecfa2d29
Jazz Kersell <jazzkersell@gmail.com>
2022-12-22 09:46:26 -0500

    2022/6: Solve part 2
    Somehow I still got the correct answer for part 1 despite my
    implementation not being correct. Rather than caching each rune in the
    window with a boolean, it is necessary to keep track of how many times
    each rune appears in the window. With that fixed the solution to part 2
    is simply to change the length of the string of unique
    characters.

--- 2022/6/solution.go
+++ 2022/6/solution.go
@@ -8,17 +8,32 @@ import (
 )
 
 func uniqueCharsFinder(length int) func(r rune) (int, bool) {
-	window := make([]rune, 4)
-	cache := make(map[rune]bool)
+	window := make([]rune, length)
+	cache := make(map[rune]int)
 	i := 0
 	return func(r rune) (int, bool) {
 		idx := i % length
 		to_evict := window[idx]
 		window[idx] = r
-		delete(cache, to_evict)
-		cache[r] = true
+
+		if count, ok := cache[to_evict]; ok {
+			if count > 0 {
+				cache[to_evict]--
+			} else if count == 0 {
+				delete(cache, to_evict)
+			} else {
+				log.Fatal("Cache count should not be negative")
+			}
+		}
+
+		if _, ok := cache[r]; ok {
+			cache[r]++
+		} else {
+			cache[r] = 0
+		}
+
 		if len(cache) == length {
-			return i, true
+			return i + 1, true
 		}
 		i++
 		return 0, false
@@ -36,11 +51,27 @@ func main() {
 	scanner.Split(bufio.ScanRunes)
 
 	packetFinder := uniqueCharsFinder(4)
+	messageFinder := uniqueCharsFinder(14)
 
+	packetFound := false
+	packetIdx := 0
 	for scanner.Scan() {
 		r := []rune(scanner.Text())[0]
-		if i, ok := packetFinder(r); ok {
-			fmt.Println(i)
+
+		if !packetFound {
+			var ok bool
+			packetIdx, ok = packetFinder(r)
+			packetFound = packetFound || ok
+		}
+
+		messageIdx, messageFound := messageFinder(r)
+
+		if packetFound && messageFound {
+			fmt.Printf(
+				"Packet found at: %d\nMessage found at: %d\n",
+				packetIdx,
+				messageIdx,
+			)
 			break
 		}
 	}
